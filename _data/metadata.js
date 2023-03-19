85678781136271fd81df59d2298eef2a7a7f6962
Galt Khurelbaatar <kh.galt53@gmail.com>
2023-03-19 16:27:56 +0800

    tested

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
-	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
-	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
-	}
-}
+	title: "Eleventy Base Blog v8",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Galt",
+		email: "kh.galt53@gmail.com",
+		url: "https://example.com/about-me/",
+	},
+};
